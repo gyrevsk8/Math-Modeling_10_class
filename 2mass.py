e496ba01c51931967fb69e123f7a4cd590a3a51a
gyrevsk8 <gyrevsk8@gmail.com>
2019-10-29 17:18:48 +0200

    Add files via upload

--- 2mass.py
+++ 2mass.py
@@ -2,10 +2,11 @@ import numpy as np
 import random as rnd
 b=np.eye(6)
 for i in range (0,5,1):
-    b[i]=rnd.random()
+    b[i]=rnd.randint(1,100)
 def multy(a):
     """
     перемножает элементы массива
+    на вход нужный массив
     """
     k=0
     for i in range(0,5,1):
